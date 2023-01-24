9e07c4fcf4e5b83788766c2acdc606235dffe864
ejaz-cedcoss <mohdejazalam2022jun@cedcoss.com>
2023-01-24 10:59:58 +0530

    changes in sample section

---
+++ src/component/Sample.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sample = () => {
+  return (
+    <div>
+      <h1>Sample component created</h1>
+      <p>loreme ipsum</p>
+      <h2>lorem ipsum asdfkj</h2>
+    </div>
+  )
+}
+
+export default Sample
